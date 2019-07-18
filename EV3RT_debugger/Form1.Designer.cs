9ccdecfae2ab969a4da53fc12a9215a25378268d
garyo <surpace2479@gmail.com>
2019-07-18 13:47:49 +0900

    Auto stash before merge of "master" and "origin/master"

--- EV3RT_debugger/Form1.Designer.cs
+++ EV3RT_debugger/Form1.Designer.cs
@@ -29,12 +29,75 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+<<<<<<< Updated upstream
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
         }
 
         #endregion
+=======
+            this.btnStart = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.cmbPortName = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(220, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 20);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM6";
+            // 
+            // cmbPortName
+            // 
+            this.cmbPortName.FormattingEnabled = true;
+            this.cmbPortName.Location = new System.Drawing.Point(12, 12);
+            this.cmbPortName.Name = "cmbPortName";
+            this.cmbPortName.Size = new System.Drawing.Size(121, 20);
+            this.cmbPortName.TabIndex = 1;
+            this.cmbPortName.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(139, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 20);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Conect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 58);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cmbPortName);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox cmbPortName;
+        private System.Windows.Forms.Button btnConnect;
+>>>>>>> Stashed changes
     }
 }
 
--- EV3RT_debugger/Form1.cs
+++ EV3RT_debugger/Form1.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+<<<<<<< Updated upstream
+=======
+using System.IO.Ports;
+>>>>>>> Stashed changes
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +16,93 @@ namespace EV3RT_debugger
 {
     public partial class Form1 : Form
     {
+<<<<<<< Updated upstream
+=======
+        void CONSOLE_INFO(string str)
+        {
+            Console.WriteLine("[INFO]  " + str);
+        }
+
+        void CONSOLE_ERROR(string str)
+        {
+            Console.WriteLine("[ERROR] " + str);
+        }
+
+>>>>>>> Stashed changes
         public Form1()
         {
             InitializeComponent();
         }
+<<<<<<< Updated upstream
+=======
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // ポートオープン
+                serialPort1.Write("1");
+                CONSOLE_INFO("Sent start signal.");
+            }
+            catch (Exception ex)
+            {
+                CONSOLE_ERROR(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //! 利用可能なシリアルポート名の配列を取得する.
+            string[] PortList = SerialPort.GetPortNames();
+
+            cmbPortName.Items.Clear();
+
+            //! シリアルポート名をコンボボックスにセットする.
+            foreach (string PortName in PortList)
+            {
+                cmbPortName.Items.Add(PortName);
+            }
+            if (cmbPortName.Items.Count > 0)
+            {
+                cmbPortName.SelectedIndex = 0;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            // ポート名を取得
+            var port = cmbPortName.Text.ToString();
+
+            CONSOLE_INFO("Try Conect to " + port + "...");
+            
+            // serialPortの設定
+            serialPort1 = new SerialPort();
+            serialPort1.PortName = port;
+            serialPort1.BaudRate = 9600;
+            serialPort1.DataBits = 8;
+            serialPort1.Parity = Parity.None;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.Encoding = Encoding.UTF8;
+            serialPort1.WriteTimeout = 100000;
+
+            // シリアルポートに接続
+            try
+            {
+                // ポートオープン
+                serialPort1.Open();
+                CONSOLE_INFO("Connected to " + port + ".");
+            }
+            catch (Exception ex)
+            { 
+                CONSOLE_ERROR(ex.Message);
+            }
+            
+        }
+>>>>>>> Stashed changes
     }
 }
